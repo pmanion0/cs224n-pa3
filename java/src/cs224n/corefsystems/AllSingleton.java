045cf1193b2c0ff8f24d175063edc46dbdeb134b
Patrick Manion <patrick.manion@capitalone.com>
2015-10-31 18:20:39 -0500

    Adding AllSingleton baseline

--- java/src/cs224n/corefsystems/AllSingleton.java
+++ java/src/cs224n/corefsystems/AllSingleton.java
@@ -1,25 +1,32 @@
 package cs224n.corefsystems;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
 import cs224n.coref.Entity;
+import cs224n.coref.Mention;
 import cs224n.util.Pair;
 
 public class AllSingleton implements CoreferenceSystem {
 
 	@Override
 	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-
+		// No training is needed for this baseline
 	}
 
 	@Override
 	public List<ClusteredMention> runCoreference(Document doc) {
-		// TODO Auto-generated method stub
-		return null;
+		List<ClusteredMention> predMentions = new ArrayList<ClusteredMention>();
+		// Create a singleton entity/cluster for every mention
+    for (Mention m : doc.getMentions()) {
+    	predMentions.add(m.markSingleton());
+    }
+    return predMentions;
 	}
 
 }
