fbdd8b08dc4edeeda3f5ffe39c0398ca4799d979
Jie Shen <jie@Jies-MacBook-Pro.local>
2015-11-01 15:11:35 -0600

    modified format

--- java/src/cs224n/corefsystems/OneCluster.java
+++ java/src/cs224n/corefsystems/OneCluster.java
@@ -14,25 +14,20 @@ import java.util.Map;
 
 public class OneCluster implements CoreferenceSystem {
 
-	@Override
-	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		// TODO Auto-generated method stub
-            if (trainingData!=null){
-                
-            }
+  @Override
+  public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
+    if (trainingData!=null){
+    }
+  }
 
-	}
-
-	@Override
-	public List<ClusteredMention> runCoreference(Document doc) {
-              List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
-              List<Mention> mentionList = doc.getMentions();
-              Entity entity = new Entity(mentionList);
-                //(for each mention...)
-              for(Mention m : mentionList){
-                  mentions.add(m.markCoreferent(entity));
-              }
-                  return mentions;
-	}
-
+  @Override
+  public List<ClusteredMention> runCoreference(Document doc) {
+    List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+    List<Mention> mentionList = doc.getMentions();
+    Entity entity = new Entity(mentionList);
+    for(Mention m : doc.getMentions()){
+      mentions.add(m.markCoreferent(entity));
+    }
+    return mentions;
+  }
 }
